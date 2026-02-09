4bbc84e4a4446efb1ab00e7af1c667e3713dcd2b
Luis Felipe Cabral <louisfelipecabral@gmail.com>
2026-02-09 10:29:37 +0000

    Estamos conectados corretamente?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "bardoluis",
-  "appId": "1:392644795631:web:f2a27d81e3c1c93ea64f1a",
-  "apiKey": "AIzaSyCoFEvyoAcvel_GDjYZnTzcpF4Ont_0EKE",
-  "authDomain": "bardoluis-86e9e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "392644795631"
+  apiKey: "AIzaSyCoFEvyoAcvel_GDjYZnTzcpF4Ont_0EKE",
+  authDomain: "bardoluis-86e9e.firebaseapp.com",
+  databaseURL: "https://bardoluis-default-rtdb.firebaseio.com",
+  projectId: "bardoluis",
+  storageBucket: "bardoluis.firebasestorage.app",
+  messagingSenderId: "392644795631",
+  appId: "1:392644795631:web:b7fc6e28a257b669a64f1a",
+  measurementId: "G-Y732X8EEZ9"
 };
